c2bf17822a67fdd7f5a11325f4788ca11cc54538
Enki <enki@becomesovran.com>
2023-05-10 00:08:56 -0700

    php edit

--- scripts/store_data.php
+++ scripts/store_data.php
@@ -4,8 +4,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $public_key = $_POST["public_key"];
     $file = ".well-known/nostr.json";
     $data = json_decode(file_get_contents($file), true);
-    $data[$public_key] = $username;
-    file_put_contents($file, json_encode($data));
+
+    // Add username and public key to "names" section
+    $data["names"][$username] = $public_key;
+
+    // Add public key and relay address to "relays" section
+    $data["relays"][$public_key] = ["wss://nostr.sovbit.host"];
+
+    file_put_contents($file, json_encode($data, JSON_PRETTY_PRINT));
     echo "Data stored successfully";
 }
 ?>
